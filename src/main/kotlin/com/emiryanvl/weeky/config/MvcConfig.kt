f0490277579203b91a7417731f5c167c1a65652b
2oublePeace <vova.emiryan@yandex.ru>
2023-10-18 15:33:40 +0400

    Добавлен MvcConfig

---
+++ src/main/kotlin/com/emiryanvl/weeky/config/MvcConfig.kt
@@ -0,0 +1,21 @@
+package com.emiryanvl.weeky.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class MvcConfig : WebMvcConfigurer {
+    override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
+        registry.addResourceHandler("/assets/**")
+            .addResourceLocations("classpath:/static/assets/")
+        registry.addResourceHandler("/css/**")
+            .addResourceLocations("classpath:/static/css/")
+        registry.addResourceHandler("/img/**")
+            .addResourceLocations("classpath:/static/img/")
+        registry.addResourceHandler("/js/**")
+            .addResourceLocations("classpath:/static/js/")
+        registry.addResourceHandler("/scss/**")
+            .addResourceLocations("classpath:/static/scss/")
+    }
+}
