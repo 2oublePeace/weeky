cc364462dce59e37ecd53d6077fdf25394654efa
2oublePeace <vova.emiryan@yandex.ru>
2023-10-18 14:58:46 +0400

    Первоначальная фиксация

---
+++ src/main/kotlin/com/emiryanvl/weeky/WeekyApplication.kt
@@ -0,0 +1,11 @@
+package com.emiryanvl.weeky
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WeekyApplication
+
+fun main(args: Array<String>) {
+    runApplication<WeekyApplication>(*args)
+}
---
+++ src/test/kotlin/com/emiryanvl/weeky/WeekyApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.emiryanvl.weeky
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WeekyApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
