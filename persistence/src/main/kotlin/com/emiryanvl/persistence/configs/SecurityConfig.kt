a618264614c98092a33e920ad045f05671bc9643
2oublePeace <vova.emiryan@yandex.ru>
2024-03-27 19:08:48 +0400

    Перенесена реализация UserDetailsService

--- persistence/src/main/kotlin/com/emiryanvl/persistence/configs/SecurityConfig.kt
+++ persistence/src/main/kotlin/com/emiryanvl/persistence/configs/SecurityConfig.kt
@@ -1,6 +1,6 @@
 package com.emiryanvl.persistence.configs
 
-import com.emiryanvl.persistence.services.impl.UserDetailsServiceImpl
+import com.emiryanvl.persistence.repositories.UserRepository
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpMethod
@@ -8,6 +8,9 @@ import org.springframework.security.authentication.AuthenticationProvider
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.core.userdetails.UsernameNotFoundException
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
@@ -37,8 +40,21 @@ class SecurityConfig {
     fun passwordEncoder() : PasswordEncoder = BCryptPasswordEncoder()
 
     @Bean
+    fun userDetailsService(userRepository: UserRepository): UserDetailsService {
+        return UserDetailsService {
+            val user = userRepository.findByUsername(it)
+                ?: throw UsernameNotFoundException("Пользователь не найден")
+            User.builder()
+                .username(user.username)
+                .password(user.password)
+                .roles(user.role)
+                .build()
+        }
+    }
+
+    @Bean
     fun authenticationProvider(
-        userDetailsService: UserDetailsServiceImpl,
+        userDetailsService: UserDetailsService,
         passwordEncoder: PasswordEncoder
     ) : AuthenticationProvider {
         return DaoAuthenticationProvider().also {
