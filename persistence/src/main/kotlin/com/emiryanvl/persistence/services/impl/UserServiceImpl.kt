979bfa0ea78f84ad6620e2a170e679d24cfeee2f
2oublePeace <vova.emiryan@yandex.ru>
2024-04-28 12:47:54 +0400

    Revert "Добавлен breadcrumb"
    This reverts commit 5aa2fc1cfc0990c1f372975da3811b6a2e7a613d.

--- persistence/src/main/kotlin/com/emiryanvl/persistence/services/impl/UserServiceImpl.kt
+++ persistence/src/main/kotlin/com/emiryanvl/persistence/services/impl/UserServiceImpl.kt
@@ -2,11 +2,9 @@ package com.emiryanvl.persistence.services.impl
 
 import com.emiryanvl.persistence.dto.requests.UserRequest
 import com.emiryanvl.persistence.dto.responses.UserResponse
-import com.emiryanvl.persistence.entities.ArticleEntity
 import com.emiryanvl.persistence.entities.UserEntity
 import com.emiryanvl.persistence.exceptions.NotFoundException.Companion.notFoundException
 import com.emiryanvl.persistence.mappers.UserMapper
-import com.emiryanvl.persistence.repositories.ArticleRepository
 import com.emiryanvl.persistence.repositories.UserRepository
 import com.emiryanvl.persistence.services.UserService
 import org.springframework.stereotype.Service
@@ -14,7 +12,6 @@ import org.springframework.stereotype.Service
 @Service
 class UserServiceImpl(
     private val userRepository: UserRepository,
-    private val articleRepository: ArticleRepository,
     private val userMapper: UserMapper
 ) : UserService {
     override fun getUser(username: String): UserResponse {
@@ -30,7 +27,7 @@ class UserServiceImpl(
     override fun createUser(userRequest: UserRequest): UserResponse {
         return userMapper.toUserResponse(
             userRepository.save(
-                complementUser(userMapper.toUserEntity(userRequest))
+                userMapper.toUserEntity(userRequest)
             )
         )
     }
@@ -47,18 +44,6 @@ class UserServiceImpl(
     }
 
     override fun deleteUser(id: Long) = userRepository.deleteById(id)
-
-    private fun complementUser(userEntity: UserEntity): UserEntity {
-        /*articleRepository.save(
-            ArticleEntity(
-                "Главная страница",
-                "/${userEntity.username}/home",
-                "/${userEntity.username}",
-                "Главная страница"
-            )
-        )*/
-        return userEntity
-    }
 
     private fun editUser(userEntity: UserEntity, userRequest: UserRequest): UserEntity {
         return userEntity.apply {
