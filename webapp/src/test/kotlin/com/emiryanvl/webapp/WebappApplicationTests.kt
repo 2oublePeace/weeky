cbc06709c3155b0ce00869030be73da8f0d621be
2oublePeace <vova.emiryan@yandex.ru>
2024-05-05 01:09:24 +0400

    Добавлена переменная для изменения api данных

--- webapp/src/test/kotlin/com/emiryanvl/webapp/WebappApplicationTests.kt
+++ webapp/src/test/kotlin/com/emiryanvl/webapp/WebappApplicationTests.kt
@@ -4,10 +4,4 @@ import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class WebappApplicationTests {
-
-    @Test
-    fun contextLoads() {
-    }
-
-}
+class WebappApplicationTests {}
