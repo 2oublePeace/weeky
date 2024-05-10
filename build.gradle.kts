eca4fb2fee1dcc9f4e0a5358ef87102c83d51403
2oublePeace <vova.emiryan@yandex.ru>
2024-05-10 20:57:04 +0400

    Написаны юнит тесты для ArticleServiceImpl

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,6 +30,9 @@ subprojects {
         implementation("org.springframework.boot:spring-boot-starter-web")
         implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
         implementation("org.jetbrains.kotlin:kotlin-reflect")
+        testImplementation("io.mockk:mockk:1.13.10")
+        testImplementation("io.kotest:kotest-runner-junit5:5.6.2")
+        testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.3")
         testImplementation("org.springframework.boot:spring-boot-starter-test")
     }
 
