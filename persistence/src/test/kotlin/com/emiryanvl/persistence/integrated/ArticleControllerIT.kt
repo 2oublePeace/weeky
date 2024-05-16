b78d5c75c059becb397c26ef1116170a1665f7c7
2oublePeace <vova.emiryan@yandex.ru>
2024-05-16 05:52:06 +0400

    Добавлены интеграционные тесты для статей

--- persistence/src/test/kotlin/com/emiryanvl/persistence/integrated/ArticleControllerIT.kt
+++ persistence/src/test/kotlin/com/emiryanvl/persistence/integrated/ArticleControllerIT.kt
@@ -1,7 +1,9 @@
 package com.emiryanvl.persistence.integrated
 
+import com.emiryanvl.persistence.dto.requests.ArticleRequest
 import com.emiryanvl.persistence.dto.requests.UserRequest
 import com.fasterxml.jackson.databind.ObjectMapper
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
@@ -9,23 +11,22 @@ import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType.APPLICATION_JSON
 import org.springframework.test.annotation.DirtiesContext
 import org.springframework.test.web.servlet.MockMvc
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import java.sql.Date
+import java.time.LocalDate
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
-class UserControllerIT {
+class ArticleControllerIT {
 
     @Autowired
     lateinit var mockMvc: MockMvc
 
-    @Test
-    fun createUserTest() {
+    @BeforeEach
+    fun createHomeArticle() {
         val request = UserRequest("user", "password", "USER")
 
         mockMvc.perform(
@@ -36,12 +37,36 @@ class UserControllerIT {
     }
 
     @Test
-    fun getUserTest() {
-        var id: Long? = null
-        val request = UserRequest("user", "password", "USER")
+    fun createArticleTest() {
+
+        val request = ArticleRequest(
+            "Футбол",
+            "/user/home/football",
+            "/user/home",
+            "**Футбольный контент**",
+            Date.valueOf(LocalDate.now())
+        )
 
         mockMvc.perform(
-            post("/user")
+            post("/article")
+                .contentType(APPLICATION_JSON)
+                .content(ObjectMapper().writeValueAsString(request))
+        ).andExpect(status().isCreated)
+    }
+
+    @Test
+    fun getArticleTest() {
+        var id: Long? = null
+        val request = ArticleRequest(
+            "Футбол",
+            "/user/home/football",
+            "/user/home",
+            "**Футбольный контент**",
+            Date.valueOf(LocalDate.now())
+        )
+
+        mockMvc.perform(
+            post("/article")
                 .contentType(APPLICATION_JSON)
                 .content(ObjectMapper().writeValueAsString(request))
         )
@@ -51,7 +76,7 @@ class UserControllerIT {
             }
 
         mockMvc.perform(
-            get("/user/${request.username}")
+            get("/article/${request.link}")
                 .contentType(APPLICATION_JSON)
         )
             .andExpect(status().isOk)
@@ -59,13 +84,25 @@ class UserControllerIT {
     }
 
     @Test
-    fun updateUserTest() {
+    fun updateArticleTest() {
         var id: Long? = null
-        val createRequest = UserRequest("user", "password", "USER")
-        val updateRequest = UserRequest("updatedUser", "updatedPassword", "USER")
+        val createRequest = ArticleRequest(
+            "Футбол",
+            "/user/home/soccer",
+            "/user/home",
+            "**Футбольный контент**",
+            Date.valueOf(LocalDate.now())
+        )
+        val updateRequest = ArticleRequest(
+            "Хоккей",
+            "/user/home/hockey",
+            "/user/home",
+            "**Хоккейный контент**",
+            Date.valueOf(LocalDate.now())
+        )
 
         mockMvc.perform(
-            post("/user")
+            post("/article")
                 .contentType(APPLICATION_JSON)
                 .content(ObjectMapper().writeValueAsString(createRequest))
         )
@@ -75,23 +112,31 @@ class UserControllerIT {
             }
 
         mockMvc.perform(
-            put("/user/$id")
+            put("/article/$id")
                 .contentType(APPLICATION_JSON)
                 .content(ObjectMapper().writeValueAsString(updateRequest))
         )
             .andExpect(status().isOk)
-            .andExpect(jsonPath("$.username").value(updateRequest.username))
-            .andExpect(jsonPath("$.password").value(updateRequest.password))
-            .andExpect(jsonPath("$.role").value(updateRequest.role))
+            .andExpect(jsonPath("$.title").value(updateRequest.title))
+            .andExpect(jsonPath("$.link").value(updateRequest.link))
+            .andExpect(jsonPath("$.content").value(updateRequest.content))
+            .andExpect(jsonPath("$.parentLink").value(updateRequest.parentLink))
+            .andExpect(jsonPath("$.date").value(updateRequest.date.toString()))
     }
 
     @Test
-    fun deleteUserTest() {
+    fun deleteArticleTest() {
         var id: Long? = null
-        val request = UserRequest("user", "password", "USER")
+        val request = ArticleRequest(
+            "Футбол",
+            "/user/home/football",
+            "/user/home",
+            "**Футбольный контент**",
+            Date.valueOf(LocalDate.now())
+        )
 
         mockMvc.perform(
-            post("/user")
+            post("/article")
                 .contentType(APPLICATION_JSON)
                 .content(ObjectMapper().writeValueAsString(request))
         )
@@ -101,7 +146,7 @@ class UserControllerIT {
             }
 
         mockMvc.perform(
-            delete("/user/$id")
+            delete("/article/$id")
                 .contentType(APPLICATION_JSON)
         ).andExpect(status().isNoContent)
     }
